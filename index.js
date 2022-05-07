fdcb6ebeb88f5b8a2c37a43f6157c9d72decbe13
aiswarya <aishdiksha@gmail.com>
2022-05-07 16:12:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+
+
+app.get('/home', (req, res) => {
+    console.log('request at home');
+    res.send('the wait is over');
+});
+
+app.listen(port, () => {
+        console.log('Server started');
+});
+
+
+
